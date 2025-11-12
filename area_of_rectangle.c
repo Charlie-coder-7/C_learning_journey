73cca0e3a5008062d446e6dabe0be05fcf4e1f53
Charlie-coder-7 <sahooachyuta168@gmail.com>
2025-11-12 22:29:25 +0530

    Add files via upload

---
+++ area_of_rectangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    //int length = 3;
+    //int breadth = 6;
+    int length,breadth;
+    printf("Enter length:");
+    scanf("%d",&length);
+    printf("Enter beadth:");
+    scanf("%d",&breadth);
+    printf("The area of this rectangle is %d\n", length*breadth);
+    return 0;
+}
---
+++ simple_interest.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    float p = 55.2;
+    int r = 6;
+    int t = 7;
+    printf("The value of simple interest is:%f\n",p*r*t/100);
+    return 0;
+}
