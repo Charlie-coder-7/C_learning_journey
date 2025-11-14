6c928b4ab56a373ec04235b84904bec48531e2bb
Charlie-coder-7 <sahooachyuta168@gmail.com>
2025-11-14 22:50:23 +0530

    Add files via upload

---
+++ divisibleby.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 2847759;
+    printf("The value if a is divisble by 97 is %d:",a%97);
+    return 0;
+}
---
+++ stepbystepevaluationexplained.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 2, y = 3, z = 3, k = 1;
+    int e = 3 * x / y - z + k;
+    printf("The value of is %d",e);
+    // 3 * x / y - z + k;
+    // 6 / y - z + k;
+    // 2 - z + k;
+    // 2 - z + k;
+    // -1 + 1 = 0
+    return 0;
+}
