b8aebe94045485cc9b8bfb5e5489df86628ec469
ACHYUTA SAHOO <sahooachyuta168@gmail.com>
2026-01-02 21:18:14 +0530

    Add files via upload

---
+++ 02_Printing_Strings.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    char st[]={"Achyuta"};//same as below declaration
+    // char st[]={'a','c','h','y','u','t','a','\0'};
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     printf("%c",st[i]);
+    // }
+    printf("%s",st);
+    return 0;
+}
